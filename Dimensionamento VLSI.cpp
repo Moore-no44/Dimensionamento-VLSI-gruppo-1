6bdfb57ecb4bc2bbb70f6e7e579ab21e5d6cd5a2
Moreno <morenotomaghelli44@gmail.com>
2026-05-20 17:31:50 +0200

    Aggiunti calcoli di capacità per ridurre la frequenza di oscillazione con target 100KHz

--- Dimensionamento VLSI.cpp
+++ Dimensionamento VLSI.cpp
@@ -7,6 +7,8 @@
 
 double V_DD = 0.3; // Tensione di alimentazione
 
+double N_inv = 5; // Numero di stadi dell'inverter
+
 double C_L = 0.5e-12; // Capacità di carico
 
 double L_min = 0.18e-6; // Lunghezza minima del canale
@@ -15,19 +17,25 @@ double W_min = 0.22e-6; // Larghezza minima del canale
 
 double C_ox = 7e-3; // Capacità di ossido per unità di area
 
+double mu_np = 3; // rapporto tra mobilità elettronica e mobilità di lacune
+
+double mu_n_C_ox = 320e-6; // prodotto tra mobilità elettronica e capacità di ossido per unità di area
+
+double V_th = 0.45; // Tensione di soglia
+
+double Frequency_target = 1e5; // Frequenza di oscillazione target
+
 int main()
 {
 	// parametri da dimensionare
 
 	double W_nand_p, W_nand_n, L_nand_p, L_nand_n;
 
-	double W_inv_p, W_inv_n, L_inv_p, L_inv_n;
-
 	double W_tbuff_1_p, W_tbuff_1_n, L_tbuff_1_p, L_tbuff_1_n;
 
-	double C_GS_13, C_GS_14;
+	double C_GS_1_tb_n, C_GS_1_tb_p;
 
-	double C_GD_13, C_GD_14;
+	double C_GD_1_tb_n, C_GD_1_tb_p;
 
 	double C_in_tbuff;
 
@@ -39,27 +47,49 @@ int main()
 
 	double K;
 
+	double a;
+
+	double C_DB_inv_n_prev, C_DB_inv_p_prev;
+	
+	double C_GD_inv_n_prev, C_GD_inv_p_prev;
+
+	double C_GS_inv_n_next, C_GS_inv_p_next;
+
+	double C_line;
+
+	double W_inv_p, W_inv_n, L_inv_p, L_inv_n;
+
+	double C_tot_inv;
+
+	double tao_inv;
+
+	double oscillation_frequency; 
+
+	double C_tot_new;
+
 	// Calcoli
 
-	W_nand_p = W_nand_n = W_min;
+	W_nand_n = W_min;
+
+	W_nand_p = 2 * W_min / 3;
 
 	L_nand_p = L_nand_n = L_min;
 
 	W_tbuff_1_n = W_min;
 
-	W_tbuff_1_p = 2 * W_min;
+	W_tbuff_1_p = 3 * W_min;
 
 	L_tbuff_1_p = L_tbuff_1_n = L_min;
 
-	C_GS_13 = 2 * C_ox * W_tbuff_1_p * L_tbuff_1_p / 3;
+	C_GS_1_tb_p = 2 * C_ox * W_tbuff_1_p * L_tbuff_1_p / 3;
 
-	C_GS_14 = 2 * C_ox * W_tbuff_1_n * L_tbuff_1_n / 3;
+	C_GS_1_tb_n = 2 * C_ox * W_tbuff_1_n * L_tbuff_1_n / 3;
 
-	C_GD_13 = C_ox * W_tbuff_1_p * L_tbuff_1_p;
+	C_GD_1_tb_p = C_ox * W_tbuff_1_p * L_tbuff_1_p;
 
-	C_GD_14 = C_ox * W_tbuff_1_n * L_tbuff_1_n;
+	C_GD_1_tb_n = C_ox * W_tbuff_1_n * L_tbuff_1_n;
 
-	C_in_tbuff = C_GS_13 + C_GS_14 + 2 * C_GD_13 + 2 * C_GD_14;
+	C_in_tbuff = C_GS_1_tb_p + C_GS_1_tb_n + 2 * C_GD_1_tb_p + 2 * C_GD_1_tb_n;
 
 	K_alla_N = C_L / C_in_tbuff;
 
@@ -118,17 +148,49 @@ int main()
 		Dimensioni[i] = 220e-9 * pow(K, i);
 	}
 
+	L_inv_n = L_inv_p = L_min;
+
+	W_inv_n = W_min;
+
+	W_inv_p = 3 * W_min;
+
+	C_GS_inv_n_next = 2 * C_ox * W_inv_n * L_inv_n / 3;
+
+	C_GS_inv_p_next = 2 * C_ox * W_inv_p * L_inv_p / 3;
+
+	C_GD_inv_n_prev = C_ox * W_inv_n * L_inv_n;
+
+	C_GD_inv_p_prev = C_ox * W_inv_p * L_inv_p;
+
+	C_DB_inv_n_prev = 5e-16 * W_inv_n * L_inv_n;
+
+	C_DB_inv_p_prev = 5e-16 * W_inv_p * L_inv_p;
+
+	C_line = 0;
+
+	C_tot_inv = C_DB_inv_n_prev + C_DB_inv_p_prev + 4 * (C_GD_inv_n_prev + C_GD_inv_p_prev) + C_GS_inv_n_next + C_GS_inv_p_next + C_line;
+
+	tao_inv = C_tot_inv * V_DD * L_inv_n / (W_inv_n * mu_n_C_ox * (V_DD - V_th) * (V_DD - V_th));
+
+	oscillation_frequency = 1 / (2 * tao_inv * N_inv);
+
+
+
+	C_tot_new = mu_n_C_ox * W_inv_n * (V_DD - V_th) * (V_DD - V_th) / 2 * N_inv * Frequency_target * V_DD * L_inv_n;
+
+	C_line = C_tot_new - C_tot_inv;
+
     // Valori ottenuti
 
 	std::cout << "NAND: W_p = " << W_nand_p << " m, L_p = " << L_nand_p << " m\n";
 
-	std::cout << "C_GS_13: " << C_GS_13 << " F\n";
+	std::cout << "C_GS_1_tb_p: " << C_GS_1_tb_p << " F\n";
 
-	std::cout << "C_GS_14: " << C_GS_14 << " F\n";
+	std::cout << "C_GS_1_tb_n: " << C_GS_1_tb_n << " F\n";
 
-	std::cout << "C_GD_13: " << C_GD_13 << " F\n";
+	std::cout << "C_GD_1_tb_p: " << C_GD_1_tb_p << " F\n";
 
-	std::cout << "C_GD_14: " << C_GD_14 << " F\n";
+	std::cout << "C_GD_1_tb_n: " << C_GD_1_tb_n << " F\n";
 
 	std::cout << "C_in_tbuff: " << C_in_tbuff << " F\n";
 
@@ -143,6 +205,18 @@ int main()
 		std::cout << "p: " << i+1 << " => " << Dimensioni[i] * 3 << " m\n";
 		std::cout << "n: " << i+1 << " => " << Dimensioni[i] << " m\n\n";
 	}
+
+	std::cout << "C_tot_inv: " << C_tot_inv << " F\n";
+
+	std::cout << "tao_inv: " << tao_inv << " s\n";
+
+	std::cout << "Oscillation Frequency: " << oscillation_frequency << " Hz\n";
+
+	std::cout << "\n";
+
+	std::cout << "C_line: " << C_line << " F\n";
+
+	std::cout << "C_tot_new: " << C_tot_new << " F\n";
 
     std::cout << "Hello World!\n";
 
